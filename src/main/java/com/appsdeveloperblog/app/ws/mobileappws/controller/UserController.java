b539d94a7891fdd900132309569ddeba88c66306
한상현 <luck2901@naver.com>
2021-08-05 13:39:17 +0900

    Initial commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/mobileappws/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.appsdeveloperblog.app.ws.mobileappws.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users/
+public class UserController {
+
+    @GetMapping
+    public String getUser()
+    {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody UserDetailsRequestModel userDetails)
+    {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser()
+    {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser()
+    {
+        return "delete user was called";
+    }
+}
